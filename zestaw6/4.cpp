f9a2fb619cc4c567d45814eafda2227eb587cf6f
Bartłomiej Grzesik <grzechovsky@gmail.com>
2019-11-20 00:12:31 +0100

    6.4 (Reccurence)

--- zestaw6/4.cpp
+++ zestaw6/4.cpp
@@ -13,33 +13,30 @@
  Do funkcji należy przekazać wyłącznie tablicę, funkcja powinna zwrócić wartość typu bool.
 */
 
-int waga(int n)
+int waga(int n, int d)
 {
-
-    if (n == 0) {
+    if (n <= 1) {
         return 0;
     }
-    int dzielnik = 2;
 
-    int c = n % 2 == 0 ? 1 : 0;
+    int r = 0;
 
-    while (n != 1) {
-        while (n % dzielnik == 0) {
-            n /= dzielnik;
-        }
-
-        if (n == 1) {
-            break;
-        }
-
-        while (n % dzielnik != 0) {
-            dzielnik++;
-        }
-
-        c++;
+    if (n % d == 0) {
+        r = 1;
+        do {
+            n /= d;
+        } while (n % d == 0);
     }
 
-    return c;
+    if (n <= 1) {
+        return r;
+    }
+
+    while (n % d != 0) {
+        d++;
+    }
+
+    return r + waga(n, d);
 }
 
 bool mozna(int t[], int n)
@@ -48,7 +45,7 @@ bool mozna(int t[], int n)
     int i_waga = 0;
 
     for (int i = 1; i < n; ++i) {
-        int c_waga = waga(t[i]);
+        int c_waga = waga(t[i], 2);
 
         int w = 0;
         while (wagi[w] != c_waga) {
@@ -71,14 +68,14 @@ bool mozna(int t[], int n)
 
 int main()
 {
-    std::cout << "waga(1) = " << waga(1) << " (=0)" << std::endl;
-    std::cout << "waga(2) = " << waga(2) << " (=1)" << std::endl;
-    std::cout << "waga(6) = " << waga(6) << " (=2)" << std::endl;
-    std::cout << "waga(30) = " << waga(30) << " (=3)" << std::endl;
-    std::cout << "waga(64) = " << waga(64) << " (=1)" << std::endl;
+    std::cout << "waga(1) = " << waga(1, 2) << " (=0)" << std::endl;
+    std::cout << "waga(2) = " << waga(2, 2) << " (=1)" << std::endl;
+    std::cout << "waga(6) = " << waga(6, 2) << " (=2)" << std::endl;
+    std::cout << "waga(30) = " << waga(30, 2) << " (=3)" << std::endl;
+    std::cout << "waga(64) = " << waga(64, 2) << " (=1)" << std::endl;
 
-    int liczby[] = { 2, 6, 30, 64 };
+    int liczby[] = { 2, 6, 30, 64, 1 };
     std::cout << mozna(liczby, 4) << std::endl;
-    int liczby2[] = { 2, 6, 30, 64, 1 };
-    std::cout << mozna(liczby2, 5) << std::endl;
+    //int liczby2[] = { 2, 6, 30, 64, 1 };
+    std::cout << mozna(liczby, 5) << std::endl;
 }
