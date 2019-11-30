35bddcb76fc45e5979d3ed5cfb98e6fcc05b99f3
Bartłomiej Grzesik <grzechovsky@gmail.com>
2019-11-30 13:21:37 +0100

    det

---
+++ inne/det.cpp
@@ -0,0 +1,143 @@
+//
+// Created by Bartłomiej Grzesik on 30/11/2019.
+//
+
+
+
+#include <iostream>
+
+template<int N>
+int det(int mat[N][N], bool col[N], bool row[N], int n = N)
+{
+    /*
+        | ...       |
+        | a0 ... a1 |
+        | ...   ... |
+        | a2 ... a3 |
+        | ...       |
+     */
+    if (n == 2) {
+        int x = 0;
+        int y = 0;
+
+        while (x < N && col[x]) { x++; }
+        while (y < N && row[y]) { y++; }
+
+        if (x >= N || y >= N) {
+            throw 10;
+        }
+
+        int a0 = mat[y][x];
+        int x0 = x++;
+
+        while (x < N && col[x]) { x++; }
+        int a1 = mat[y++][x];
+
+        while (y < N && row[y]) { y++; }
+        int a3 = mat[y][x];
+
+        int a2 = mat[y][x0];
+
+        int det = a0 * a3 - a1 * a2;
+
+        printf("| %d %d |\n", a0, a1);
+        printf("| %d %d |\n", a2, a3);
+        printf("  = %d\n", det);
+
+        return det;
+    }
+
+    int sum = 0;
+    int y = 0;
+    while (y < N && row[y]) { y++; }
+
+    row[y] = true;
+
+    int ix = 0;
+
+    for (int x = 0; x < N; x++) {
+        if (col[x]) continue;
+
+        ix++;
+
+        col[x] = true;
+
+        int minor = det<N>(mat, col, row, n - 1);
+        if (ix % 2 == 1) {
+            sum += mat[y][x] * minor;
+        } else {
+            sum -= mat[y][x] * minor;
+        }
+
+
+        col[x] = false;
+    }
+    row[y] = false;
+
+    for (int y = 0; y < N; ++y) {
+        if (row[y]) continue;
+
+        printf("|");
+        for (int x = 0; x < N; ++x) {
+            if (col[x]) continue;
+
+            printf(" %d ", mat[y][x]);
+        }
+        printf("|\n");
+    }
+    printf(" = %d\n", sum);
+
+    return sum;
+}
+
+int main()
+{
+    int mat2[2][2] = {
+        { 1, 1 },
+        { 1, 1 }
+    };
+
+    bool col2[2] { false, false };
+    bool row2[2] { false, false };
+
+    std::cout << det<2>(mat2, col2, row2) << std::endl;
+
+
+    int mat3[3][3] = {
+        { 1, 0, 0 },
+        { 0, 1, 0 },
+        { 0, 0, 1 },
+    };
+
+    bool col3[3] { false, false, false };
+    bool row3[3] { false, false, false };
+
+    std::cout << det<3>(mat3, col3, row3);
+
+    {
+        int mat4[4][4] = {
+            { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 0, 1 },
+        };
+
+        bool col4[4] { false, false, false, false };
+        bool row4[4] { false, false, false, false };
+
+        std::cout << det<4>(mat4, col4, row4) << std::endl;
+    }
+    {
+        int mat4[4][4] = {
+            { 1, 3, 5, 9 },
+            { 1, 3, 1, 7 },
+            { 4, 3, 9, 7 },
+            { 5, 2, 0, 9 },
+        };
+
+        bool col4[4] { false, false, false, false };
+        bool row4[4] { false, false, false, false };
+
+        std::cout << det<4>(mat4, col4, row4) << std::endl;
+    }
+}
