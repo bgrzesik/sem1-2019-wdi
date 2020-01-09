ce65ec4ced0dfc4f3d64a09b20996b746135ab6e
Bartłomiej Grzesik <grzechovsky@gmail.com>
2020-01-09 14:15:22 +0100

    Detect a loop in cyclic list

---
+++ inne/cycling_list.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Bartłomiej Grzesik on 09/01/2020.
+//
+
+#include <iostream>
+
+struct node
+{
+    int val;
+    node *next;
+};
+
+bool is_cyclic(node *n)
+{
+    node *fast = n;
+    node *slow = n;
+
+    while (slow != nullptr and fast != nullptr and fast->next != nullptr) {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(int argc, const char *argv[])
+{
+    node a, b, c, d, e, f;
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+    e.next = &f;
+    f.next = &c;
+
+    std::cout << is_cyclic(&a) << std::endl;
+
+}
